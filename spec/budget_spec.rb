867d117043cbaba2aaf78e494f57400fc0f94fa3
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-13 11:55:56 -0500

    add test: for #add_department

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -4,6 +4,8 @@ require './lib/department'
 
 RSpec.describe Budget do
 let(:budget) {Budget.new(2022)}
+let(:customer_service) { Department.new("Customer Service") }
+
 
   describe '#initialize' do
     it 'exists' do
@@ -13,6 +15,14 @@ let(:budget) {Budget.new(2022)}
     it 'has attributes' do
       expect(budget.year).to eq(2022)
       expect(budget.departments).to eq([])
+    end
+  end
+
+  describe '#add_department' do
+    it 'can add departments' do
+      budget.add_department(customer_service)
+
+      expect(budget.departments).to eq([customer_service])
     end
   end
 end
