330df7f63b5b39ca19407f3bf6e134eb861bab26
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-13 11:24:49 -0500

    feat: add two new files and initialize test for department

---
+++ spec/department_spec.rb
@@ -0,0 +1,11 @@
+require './lib/employee'
+require './lib/department'
+
+RSpec.describe Employee do
+  let(:customer_service) { Department.new("Customer Service") }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(customer_service).to be_a(Department)
+    end
+  end
