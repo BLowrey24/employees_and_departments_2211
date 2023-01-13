4a4c4a680ce234ec89ae7bdc02c4fda45b48e97b
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-13 11:51:59 -0500

    add test: for #initialize and checking attributes

---
+++ spec/budget_spec.rb
@@ -0,0 +1,18 @@
+require './lib/budget'
+require './lib/employee'
+require './lib/department'
+
+RSpec.describe Budget do
+let(:budget) {Budget.new(2022)}
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(buget).to be_a(Budget)
+    end
+
+    it 'has attributes' do
+      expect(budget.year).to eq(2022)
+      expect(budget.departments).to eq([])
+    end
+  end
+end
