ad743d5cb3118562bcb0d982e354a08445597757
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-13 11:31:33 -0500

    add test: for #hire

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -3,6 +3,9 @@ require './lib/department'
 
 RSpec.describe Employee do
   let(:customer_service) { Department.new("Customer Service") }
+  let(:bobbi) { Employee.new( { name: "Bobbi Jaeger", age: "30", salary: "$100000" } ) }
+  let(:aaron) { Employee.new( { name: "Aaron Tanaka", age: "25", salary: "$90000" } ) }
+
 
   describe '#initialize' do
     it 'exists' do
@@ -12,6 +15,15 @@ RSpec.describe Employee do
     it 'has attributes' do
       expect(customer_service.name).to eq("Customer Service")
       expect(customer_service.employees).to eq([])
+    end
+  end
+
+  describe 'hire' do
+    it 'adds a new employee' do
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
